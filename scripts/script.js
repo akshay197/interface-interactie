777c67de766393f20b8dbe8857f680b4fea3a02a
akshay197 <akshay.ak@hotmail.com>
2022-05-15 18:47:03 +0200

    articles geupload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
 // JavaScript Document
-console.log("Howdy!");
+const body = document.querySelector("body");
+const ol = document.querySelector("ol");
+const items = document.querySelectorAll("main > ol li");
+const button1 = document.querySelector("main button:last-of-type");
+const button2 = document.querySelector("main button:first-of-type");
+let currentPos = 0;
+
+button1.addEventListener("click", () => {
+  if (currentPos === items.length - 1) {
+    currentPos = 0;
+  } else {
+    currentPos++;
+  }
+  const scrollpx = items[currentPos].offsetLeft;
+  body.scrollLeft = 100;
+});
+
+button2.addEventListener("click", () => {
+  if (currentPos === 0) {
+    currentPos = items.length - 1;
+  } else {
+    currentPos--;
+  }
+
+  ol.scrollLeft = items[currentPos].offsetLeft;
+});
